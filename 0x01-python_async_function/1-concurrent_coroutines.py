50be94a265e29a74a0a57ff774f5a3f036352c28
Acochisse <1819@holbertonschool.com>
2022-08-30 12:42:08 -0500

    fixes

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """runs multiple coroutines at the same time with async"""
 import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
-    """runs multiple coroutines at the same time with async"""
-    delay = []
-    for i in range(n):
-        delay.append(asyncio.sleep(max_delay))
-    return [await i for i in asyncio.as_completed(delay)]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """creates the list of delays"""
+    list_m = [await wait_random(max_delay) for i in range(n)]
+    return sorted(list_m)
