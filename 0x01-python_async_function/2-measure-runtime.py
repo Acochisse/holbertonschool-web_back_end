7e41640b14070f39603d64adf02451f90b14d55b
Acochisse <1819@holbertonschool.com>
2022-08-30 12:48:59 -0500

    named incorrectly

--- 0x01-python_async_function/2-measure-runtime.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""uses previously created functions to measure time"""
-import asyncio
-
-
-async def measure_time(n, max_delay):
-    """uses previously created functions to measure time"""
-    return asyncio.run(wait_n(n, max_delay))
