4eda2299e7bf9c5e7bbc9809f38c368de547a384
Acochisse <1819@holbertonschool.com>
2022-08-30 12:51:58 -0500

    added total time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,13 +2,13 @@
 """uses previously created functions to measure time"""
 import asyncio
 import time
-import random
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """uses previously created functions to measure time"""
-    start = time.perf_counter()
-    await wait_n(n, max_delay)
-    end = time.perf_counter()
-    return (end - start) / n
+    total_time: float
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start_time
+    return total_time / n
