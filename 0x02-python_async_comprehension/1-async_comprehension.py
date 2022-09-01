2a68e66d61480e714f2befbbc0ecb2f3ae13a94b
Acochisse <1819@holbertonschool.com>
2022-08-31 22:46:59 -0500

    removed ':' from the description

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,5 +9,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """uses async comprehension to loop for a range of 10""":
+    """uses async comprehension to loop for a range of 10"""
     return [i async for i in async_generator()]
