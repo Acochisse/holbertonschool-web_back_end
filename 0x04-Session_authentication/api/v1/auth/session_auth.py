f3941f65f3b46d9cc73b62dee1cbf162907ce084
Acochisse <1819@holbertonschool.com>
2022-09-21 13:39:27 -0500

    pycodestyle fixes

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
 
+
 class SessionAuth(Auth):
     """ SessionAuth class"""
     user_id_by_session_id = {}
@@ -17,13 +18,13 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """returns a User ID based on a Session ID"""
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """overloads Auth and retrieves the User instance for a request"""
         session_id = self.session_cookie(request)
