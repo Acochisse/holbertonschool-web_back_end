932d06d7c3287a64d3e3a217354f5bd40a81a204
Acochisse <1819@holbertonschool.com>
2022-09-21 21:29:03 -0500

    changed import order

--- 0x04-Session_authentication/api/v1/__init__.py
+++ 0x04-Session_authentication/api/v1/__init__.py
@@ -1,11 +1,8 @@
 #!usr/bin/env python3
 """Manages API routes for the SessionAuth class"""
 from flask import Blueprint, jsonify, request
-
-
-appviews = Blueprint('appviews', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.users import *
 
+appviews = Blueprint('appviews', __name__, url_prefix='/api/v1')
 User.load_from_file()
