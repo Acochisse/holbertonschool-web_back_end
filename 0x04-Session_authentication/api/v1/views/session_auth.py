adfe6421bc16511460f8178dd9ea74d0fbab1d64
Acochisse <1819@holbertonschool.com>
2022-09-21 22:25:35 -0500

    removed extra quotes from documentation

--- 0x04-Session_authentication/api/v1/views/session_auth.py
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -6,7 +6,7 @@ Module that handles all session authentication
 
 @app.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """login handles the post request for the route /auth_session/login""""
+    """login handles the post request for the route /auth_session/login"""
     request.form.get('email')
     request.form.get('password')
     if email is None:
@@ -18,7 +18,7 @@ def login():
     usr = User.search({'email': user_email})
     if not usr.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    from api.v1.auth.auth import auth
+    from api.v1.app import auth
     out = auth.create_session(usr.id)
     out = jsonify(usr.to_json())
     out.set_cookie(getenv('SESSION_NAME'), out)
