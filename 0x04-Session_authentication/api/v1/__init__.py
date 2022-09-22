c3cda106c99cd28fa078ae7a123e67533ecee883
Acochisse <1819@holbertonschool.com>
2022-09-21 22:22:29 -0500

    cleaned up

--- 0x04-Session_authentication/api/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/env python3
-"""Manages API routes for the SessionAuth class"""
-from flask import Blueprint, jsonify, request
-from api.v1.views.index import *
-from api.v1.views.users import *
-
-appviews = Blueprint('appviews', __name__, url_prefix='/api/v1')
-User.load_from_file()
