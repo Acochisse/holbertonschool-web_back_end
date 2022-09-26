b58ea1e3b93f4a1bcc955224a4b794e3e6ce5409
Acochisse <1819@holbertonschool.com>
2022-09-26 08:29:00 -0500

    adding db and user

---
+++ 0x05-user_authentication_service/README.md
@@ -0,0 +1,4 @@
+This directory is related to project 1714 - 0x05-user_authentication_service
+
+### User Authenication Service
+
---
+++ 0x05-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module that creates a SQLAlchemy model named User
+
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """model for database table"""
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
