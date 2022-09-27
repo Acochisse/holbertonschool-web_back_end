4a7a5cb9a4d9199d408e259457fb483713f3b235
Acochisse <1819@holbertonschool.com>
2022-09-27 18:39:31 -0500

    changed return on hashed_password to byte type

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -15,7 +15,7 @@ def _generate_uuid() -> str:
     return str(uuid.uuid4())
 
 
-def _hash_password(password: str) -> str:
+def _hash_password(password: str) -> byte:
     """Hash a password for storing.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
