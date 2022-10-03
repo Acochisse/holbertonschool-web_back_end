40417fc5bf8954b21cf16b16a73a419a24c1f290
Acochisse <1819@holbertonschool.com>
2022-10-03 10:08:05 -0500

    adding test_exception

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Tests the access_nested_map function in utils.py"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Tests the access_nested_map function in utils.py"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
