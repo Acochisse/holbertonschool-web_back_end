5f797c738ff3aa5805bf4e4851cbab966819f42d
Acochisse <1819@holbertonschool.com>
2022-10-06 11:31:44 -0500

    adding task 3

---
+++ 0x01-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+module that creates a simple flask app
+"""
+import flask
+from urllib import request
+from flask import render_template
+from flask_babel import Babel, gettext
+
+
+app = flask.Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@app.route('/', strict_slashes=False)
+def create_app() -> str:
+    """Creates a simple flask app"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
