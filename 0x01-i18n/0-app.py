ca6784059f9e82e664e7ad72b2673f035782a5a0
Acochisse <1819@holbertonschool.com>
2022-10-06 11:05:58 -0500

    adding 0-2

---
+++ 0x01-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module that creates a simple flask app
+"""
+import flask
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def create_app():
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x01-i18n/README.md
@@ -0,0 +1,1 @@
+This directory is related to project 1716 - 0x01-i18n
