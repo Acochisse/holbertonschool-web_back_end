25e7f65e9d98bfc18d69c4a8d14d879a0691ee3c
Acochisse <1819@holbertonschool.com>
2022-10-10 12:12:32 -0500

    adding modules

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -4,6 +4,7 @@ Module containing the class Cache, which manages a cache database with redis
 """
 import redis
 import uuid
+from typing import Union, Callable, Optional
 
 
 def call_history(method: callable) -> callable:
