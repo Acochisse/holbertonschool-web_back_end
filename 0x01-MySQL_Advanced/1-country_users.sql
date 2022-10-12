43cb3f1bb12230834283c50a6df9934120e9f211
Acochisse <1819@holbertonschool.com>
2022-10-12 08:55:18 -0500

    if not exists, uniq,

--- 0x01-MySQL_Advanced/1-country_users.sql
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -1,12 +1,12 @@
--- creates a table with the following attributes:
+-- creates a table with the following attributes if it doesnt exist:
 -- 1. id (int)
--- 2. email (varchar)
--- 3. name (varchar)
+-- 2. email (varchar, uniq)
+-- 3. name (string)
 -- 4. country, ennumeration of the countries US, CO, and TN
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
