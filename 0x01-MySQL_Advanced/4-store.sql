7ef565d0389a4561dd244120a42645ff8615312c
Acochisse <1819@holbertonschool.com>
2022-10-12 12:06:10 -0500

    changed to before insert

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,12 +1,8 @@
 --script that creates a trigger that decreases the quantity of an item
 --after a new order is placed
 
-CREATE TRIGGER decrease_quantity
-BEFORE INSERT 
-ON orders
-FOR EACH ROW
-BEGIN
-UPDATE items
+CREATE TRIGGER order_dec BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
 SET quantity = quantity - NEW.quantity
 WHERE name = NEW.item_name;
-END;
+
