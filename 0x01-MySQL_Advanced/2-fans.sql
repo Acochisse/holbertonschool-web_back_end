7670d21f86e68d863c8e455ed6a7db6770a02e2f
Acochisse <1819@holbertonschool.com>
2022-10-12 09:07:11 -0500

    adding 2

---
+++ 0x01-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+--Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+-- imported from the file metal_bands.sql
+-- Column names must be: origin and nb_fans
+
+Select DISTINCT `origin`, COUNT(`fans`) AS `nb_fans` FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
