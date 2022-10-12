483a4039cea237b542a2afa16b8649bd07c89ed0
Acochisse <1819@holbertonschool.com>
2022-10-12 11:07:43 -0500

    changed how it works

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -2,11 +2,12 @@
 --after a new order is placed
 
 CREATE TRIGGER decrease_quantity
-AFTER INSERT ON orders
+AFTER INSERT 
+ON orders
 FOR EACH ROW
 BEGIN
 UPDATE items
-SET quantity = quantity - NEW.quantity
-WHERE id = NEW.item_id;
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
 END;
 
