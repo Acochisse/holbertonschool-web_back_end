1d3a0872900e1c5145cdc98f75a32ac4efbe8c8d
Acochisse <1819@holbertonschool.com>
2022-10-14 09:34:15 -0500

    changed format and added item call'

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,10 +1,6 @@
 --script that creates a trigger that decreases the quantity of an item
 --after a new order is placed
 
-CREATE TRIGGER order_dec 
-    AFTER INSERT ON orders
-    FOR EACH ROW 
-    UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;
-
+CREATE TRIGGER order_dec AFTER INSERT ON orders FOR EACH ROW UPDATE items
+SET items.quantity = (quantity - NEW.number)
+WHERE items.name = NEW.item_name;
