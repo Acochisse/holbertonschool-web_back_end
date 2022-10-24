cf78295ae96034d401cf90835d89c6979e76f4e3
Acochisse <1819@holbertonschool.com>
2022-10-24 12:57:10 -0500

    fixes

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -9,25 +9,28 @@
 const fs = require('fs');
 
 const countStudents = (file) => {
-    if (!fs.existsSync(file)) {
-        throw new Error('Cannot load the database');
-    }
-    const students = fs.readFileSync(file, 'utf8');
-    let lines = students.split('\n');
-    lines = lines.filter((line) => !== '').slice(1);
-    console.log(`Number of students: ${lines.length}`);
-
-    const fields = lines.map((line) => line.split(',')[3]);
-    const uniqueFields = [...new Set(fields)];
-    
-    uniqueFields.forEach((field) => {
-        const studentsInField = lines
-            .filter((line => line.endsWith(field)))
-            .map((line) => {
-                const student = line.split(',');
-                return student[0];
-            });
-        console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
-    });
+  if (!fs.existsSync(file)) {
+    throw new Error('Cannot load the database');
+  }
+  const students = fs.readFileSync(file, 'utf8');
+  let lines = students.split('\n');
+  lines = lines.filter((line) => line !== '').slice(1);
+  console.log(`Number of students: ${lines.length}`);
+  const field = lines.map((line) => line.split(',')[3]);
+  const eachField = [...new Set(field)];
+  eachField.forEach((fieldName) => {
+    const studentsPerField = lines
+      .filter((line) => line.endsWith(fieldName))
+      .map((line) => {
+        const split = line.split(',');
+        return split[0];
+      });
+    console.log(
+      `Number of students in ${fieldName}: ${
+        studentsPerField.length
+      }. List: ${studentsPerField.join(', ')}`,
+    );
+  });
 };
 
+module.exports = countStudents;
