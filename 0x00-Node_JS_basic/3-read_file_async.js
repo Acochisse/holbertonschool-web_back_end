118a9b2f7d03c37968b26428044dc702661f2456
Acochisse <1819@holbertonschool.com>
2022-10-24 11:32:13 -0500

    do it again again

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -2,26 +2,38 @@
 
 const fs = require('fs');
 
-const countStudents = (path) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-        if (error) {
-            throw new Error('Cannot load the database');
-        }
-        const students = data.toString().split('\n');
-        students.shift();
-        students.pop();
-        console.log(`Number of students: ${students.length}`);
-        const subjects = {};
-        for (const row of students) {
-            const student = row.split(',');
-            if (!subjects[student[3]]) subjects[student[3]] = [];
-            subjects[student[3]].push(student[0]);
-        }
-        for (const subject in subjects) {
-            if (subject) {
-                console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
-            }
-        }
-    });
-}
+const countStudents = async (file) => {
+  let content;
+  try {
+    content = await fs.promises.readFile(file, 'utf8');
+  } catch (_error) {
+    throw new Error('Cannot load the database');
+  }
+  let lines = content.split('\n');
+  lines = lines.filter((line) => line !== '').slice(1);
+  console.log(`Number of students: ${lines.length}`);
+  const field = lines.map((line) => line.split(',')[3]);
+  const eachField = [...new Set(field)];
+  const dict = {};
+  for (let i = 0; i < eachField.length; i += 1) {
+    const numStudents = field.filter(
+      (fieldName) => fieldName === eachField[i],
+    ).length;
+    const studentsPerField = lines.filter(
+      (line) => line.split(',')[3] === eachField[i],
+    );
+    const names = studentsPerField.map((line) => line.split(',')[0]);
+    console.log(
+      `Number of students in ${
+        eachField[i]
+      }: ${numStudents}. List: ${names.join(', ')}`,
+    );
+    dict[eachField[i]] = {
+      numStudents,
+      names,
+    };
+  }
+  return dict;
+};
+
 module.exports = countStudents;
