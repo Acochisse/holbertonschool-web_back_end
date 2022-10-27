a19e7b16ea1a7e17d133eb4891e52ad31aefbfa8
Acochisse <1819@holbertonschool.com>
2022-10-26 19:47:24 -0500

    named the function correctly

--- 0x01-unittests_in_js/0-calcul.js
+++ 0x01-unittests_in_js/0-calcul.js
@@ -1,11 +1,8 @@
 // script that takes in two arguments (a, b) and returns the rounded sum of the two numbers
-function sumRound(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    return NaN;
-  }
+function calculateNumber(a, b) {
   const aR = Math.round(a);
   const bR = Math.round(b);
   return aR + bR;
 }
 
-module.exports = sumRound;
+module.exports = calculateNumber;
--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -1,46 +1,46 @@
-// script that uses mocha to test the sumRound function
+// script that uses mocha to test the calculateNumber function
 
 const assert = require('assert');
-const sumRound = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul.js');
 
-describe('sumRound', function () {
+describe('calculateNumber', function () {
   it('returns the rounded sum of the two positive numbers', function () {
-    assert.strictEqual(sumRound(1.4, 4.5), 6);
-    assert.strictEqual(sumRound(4.5, 1.4), 6);
-    assert.strictEqual(sumRound(1.5, 3.5), 6);
-    assert.strictEqual(sumRound(3.5, 1.5), 6);
-    assert.strictEqual(sumRound(1.5, 4.5), 7);
-    assert.strictEqual(sumRound(4.5, 1.5), 7);
-    assert.strictEqual(sumRound(1.4, 4.4), 5);
-    assert.strictEqual(sumRound(4.4, 1.4), 5);
-    assert.strictEqual(sumRound(1.4, 4.6), 6);
-    assert.strictEqual(sumRound(4.6, 1.4), 6);
-    assert.strictEqual(sumRound(1.3, 4.3), 5);
-    assert.strictEqual(sumRound(4.3, 1.3), 5);
-    assert.strictEqual(sumRound(1.3, 4.7), 6);
-    assert.strictEqual(sumRound(4.7, 1.3), 6);
+    assert.strictEqual(calculateNumber(1.4, 4.5), 6);
+    assert.strictEqual(calculateNumber(4.5, 1.4), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+    assert.strictEqual(calculateNumber(3.5, 1.5), 6);
+    assert.strictEqual(calculateNumber(1.5, 4.5), 7);
+    assert.strictEqual(calculateNumber(4.5, 1.5), 7);
+    assert.strictEqual(calculateNumber(1.4, 4.4), 5);
+    assert.strictEqual(calculateNumber(4.4, 1.4), 5);
+    assert.strictEqual(calculateNumber(1.4, 4.6), 6);
+    assert.strictEqual(calculateNumber(4.6, 1.4), 6);
+    assert.strictEqual(calculateNumber(1.3, 4.3), 5);
+    assert.strictEqual(calculateNumber(4.3, 1.3), 5);
+    assert.strictEqual(calculateNumber(1.3, 4.7), 6);
+    assert.strictEqual(calculateNumber(4.7, 1.3), 6);
   });
   it('returns a rounded sum of blended positive and negative numbers', function () {
-    assert.strictEqual(sumRound(-1.6, 4.4), 2);
-    assert.strictEqual(sumRound(4.4, -1.6), 2);
-    assert.strictEqual(sumRound(2.3, -4.4), -2);
-    assert.strictEqual(sumRound(-4.4, 2.3), -2);
+    assert.strictEqual(calculateNumber(-1.6, 4.4), 2);
+    assert.strictEqual(calculateNumber(4.4, -1.6), 2);
+    assert.strictEqual(calculateNumber(2.3, -4.4), -2);
+    assert.strictEqual(calculateNumber(-4.4, 2.3), -2);
   });
   it('returns NaN if a or b is not a number', function () {
-    assert.strictEqual(sumRound('A', 1.6), NaN);
-    assert.strictEqual(sumRound(1.6, 'B'), NaN);
-    assert.strictEqual(sumRound('A', 'B'), NaN);
+    assert.strictEqual(calculateNumber('A', 1.6), NaN);
+    assert.strictEqual(calculateNumber(1.6, 'B'), NaN);
+    assert.strictEqual(calculateNumber('A', 'B'), NaN);
   });
   it('returns NaN if a or b is infinite', function () {
-    assert.strictEqual(sumRound(Infinity, 1.6), Infinity);
-    assert.strictEqual(sumRound(1.6, Infinity), Infinity);
-    assert.strictEqual(sumRound(Infinity, Infinity), Infinity);
-    assert.strictEqual(sumRound(-Infinity, 1.6), -Infinity);
-    assert.strictEqual(sumRound(1.6, -Infinity), -Infinity);
-    assert.strictEqual(sumRound(-Infinity, -Infinity), -Infinity);
+    assert.strictEqual(calculateNumber(Infinity, 1.6), Infinity);
+    assert.strictEqual(calculateNumber(1.6, Infinity), Infinity);
+    assert.strictEqual(calculateNumber(Infinity, Infinity), Infinity);
+    assert.strictEqual(calculateNumber(-Infinity, 1.6), -Infinity);
+    assert.strictEqual(calculateNumber(1.6, -Infinity), -Infinity);
+    assert.strictEqual(calculateNumber(-Infinity, -Infinity), -Infinity);
   });
   it('returns rounded sum of negative integers', function () {
-    assert.strictEqual(sumRound(-1, -4), -5);
-    assert.strictEqual(sumRound(-4, -1), -5);
+    assert.strictEqual(calculateNumber(-1, -4), -5);
+    assert.strictEqual(calculateNumber(-4, -1), -5);
   });
 });
