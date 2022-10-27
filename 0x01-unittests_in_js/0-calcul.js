cd65095e33b5aa90919c446fa06b77f816b76101
Acochisse <1819@holbertonschool.com>
2022-10-27 12:17:21 -0500

    removed if statement about NaN

--- 0x01-unittests_in_js/0-calcul.js
+++ 0x01-unittests_in_js/0-calcul.js
@@ -1,8 +1,5 @@
 // script that takes in two arguments (a, b) and returns the rounded sum of the two numbers
 function calculateNumber(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return NaN;
-  }
   return Math.round(a) + Math.round(b);
 }
 
