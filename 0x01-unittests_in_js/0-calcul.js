653bc8d7071de11906fd8f6f95fa4e81132a1e42
Acochisse <1819@holbertonschool.com>
2022-10-26 19:54:03 -0500

    adding NaN return

--- 0x01-unittests_in_js/0-calcul.js
+++ 0x01-unittests_in_js/0-calcul.js
@@ -1,5 +1,8 @@
 // script that takes in two arguments (a, b) and returns the rounded sum of the two numbers
 function calculateNumber(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
   const aR = Math.round(a);
   const bR = Math.round(b);
   return aR + bR;
