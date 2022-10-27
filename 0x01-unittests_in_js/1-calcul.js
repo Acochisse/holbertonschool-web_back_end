e19e8bd00ed7501b80019646b216b72751d9b1a5
Acochisse <1819@holbertonschool.com>
2022-10-27 13:04:21 -0500

    put type in the right order

--- 0x01-unittests_in_js/1-calcul.js
+++ 0x01-unittests_in_js/1-calcul.js
@@ -1,19 +1,17 @@
-// script that takes in the arguements (a, b) and returns the rounded sum of the two numbers
-// also takes in the third argument (type) and performs the operation specified by the type
-
-function calculateNumber(a, b, type) {
- if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  } else if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  } else if (type === 'DIVIDE') {
-    if (Math.round(b) === 0) {
-      return 'Error';
-    } else {
+function calculateNumber(type, a, b) {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+    case 'DIVIDE':
+      if (Math.round(b) === 0) return 'Error';
       return Math.round(a) / Math.round(b);
-    }
-  } else if (type === 'MULTIPLY') {
-    return Math.round(a) * Math.round(b);
-  }
+    case 'MULTIPLY':
+      return Math.round(a) * Math.round(b);
+    default:
+      throw new TypeError;
+  };
 }
-  module.exports = calculateNumber;
+
+module.exports = calculateNumber;
