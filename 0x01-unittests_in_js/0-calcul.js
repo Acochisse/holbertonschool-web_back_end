d0912199289d1d688653ddef81ca219be48ea910
Acochisse <1819@holbertonschool.com>
2022-10-26 20:17:03 -0500

    re rounded a+b after

--- 0x01-unittests_in_js/0-calcul.js
+++ 0x01-unittests_in_js/0-calcul.js
@@ -5,7 +5,7 @@ function calculateNumber(a, b) {
   }
   const aR = Math.round(a);
   const bR = Math.round(b);
-  return aR + bR;
+  return Math.round(aR + bR);
 }
 
 module.exports = calculateNumber;
