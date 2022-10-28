b50fca2e6a77f115495ff18f10aa1461c9fc536b
Acochisse <1819@holbertonschool.com>
2022-10-28 10:35:38 -0500

    again

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -1,43 +1,24 @@
-// this script creates a small server using the http module
-// it should be assigned to a variable named app
-// it should listen on port 1245
-// it should respond to GET requests with a body of Holberton School
-
 const http = require('http');
 const countStudents = require('./3-read_file_async');
-const DATABASE = process.argv[2];
-
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-
-	const { url } = req;
-	if (url === '/') {
+const app = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	if (req.url === '/') {
 		res.write('Hello Holberton School!');
-	
-	if (url === '/students') {
-		res.write('This is the list of our students\n');
-    try {
-      const returnValue = await countStudents(DATABASE);
-      res.write(`Number of students: ${returnValue.students.length}\n`);
-      res.write(
-        `Number of students in CS: ${
-          returnValue.studentsByCS.length
-        }. List: ${returnValue.studentsByCS.join(", ")}\n`
-      );
-      res.write(
-        `Number of students in SWE: ${
-          returnValue.studentsBySWE.length
-        }. List: ${returnValue.studentsBySWE.join(", ")}`
-      );
-    } catch (error) {
-      res.statusCode = 404;
-      res.end(error.message);
-    }
-  }
-  res.end();
+	}
+	if (req.url === '/students') {
+		res.write('This is the list of our students')
+		try {
+			const path = process.argv[2];
+			let slist = countStudents(path);
+			res.end(String(slist));
+		} catch (err) {
+			res.end(err.message);
+		}
+	}
+	res.end();
 });
-	
+
 app.listen(port);
+module.exports = app;
