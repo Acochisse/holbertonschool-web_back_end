1b700209fee923ea9c2f2373a108e3f1e1bc523d
Acochisse <1819@holbertonschool.com>
2022-10-28 14:25:23 -0500

    changed the /students route

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -2,23 +2,20 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async(req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	if (req.url === '/') {
 		res.write('Hello Holberton School!');
 	}
 	if (req.url === '/students') {
-		countStudents(process.argv[2])
-			.then((data) => {
-				res.write(`This is the list of our students' + ${data}`);
-			})
-			.catch((err) => {
-				res.write(err.message);
-			});
-	}
-
-	res.end();
-});
+		res.write("This is the list of our students\n");
+		try {
+			const students = await countStudents(process.argv[2]);
+			res.end(`${students.join('\n')}`);
+		} catch (err){
+				res.end(err.message);
+		}
+	}});
 
 app.listen(port);
 module.exports = app;
