6508c65b484cca6d0ae998b441099815bb5492ad
Acochisse <1819@holbertonschool.com>
2022-10-28 10:20:00 -0500

    update

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -5,49 +5,39 @@
 
 const http = require('http');
 const countStudents = require('./3-read_file_async');
+const DATABASE = process.argv[2];
 
 const port = 1245;
-const app = http
-	.createServer(async (req, res) => {
-	if (req.url === '/') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/plain');
-			res.write('Hello Holberton School!');
-	} else if (req.url === '/students') {
-			res.statusCode = 200;
-			res.write('This is the list of our students\n');
 
-			await countStudents(process.argv[2])
-			.then((data) => {
-					const fields = Object.keys(data);
+const app = http.createServer(async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
 
-					const total = fields.reduce(
-							(acc, curr) => acc + data[curr].numStudents,
-							0,
-					);
-
-					res.write(`Number of students: ${total}\n`);
-					for (let i = 0; i < fields.length; i += 1) {
-							res.write(
-									`Number of students in ${fields[i]}: ${
-											data[fields[i]].numStudents
-									}. `,
-							);
-							res.write(`List: ${data[fields[i]].names.join(', ')}`);
-
-							if (i !== fields.length - 1) {
-									res.write('\n');
-							}
-					}
-			})
-			.catch((err) => {
-					res.write(err.message);
-			});
-			app.finally(() => {
-					res.end();
-			});
-	}
-	})
-	app.listen(port);
-
-module.exports = app;
+	const { url } = req;
+	if (url === '/') {
+		res.write('Hello Holberton School!');
+	
+	if (url === '/students') {
+		res.write('This is the list of our students\n');
+    try {
+      const returnValue = await countStudents(DATABASE);
+      res.write(`Number of students: ${returnValue.students.length}\n`);
+      res.write(
+        `Number of students in CS: ${
+          returnValue.studentsByCS.length
+        }. List: ${returnValue.studentsByCS.join(", ")}\n`
+      );
+      res.write(
+        `Number of students in SWE: ${
+          returnValue.studentsBySWE.length
+        }. List: ${returnValue.studentsBySWE.join(", ")}`
+      );
+    } catch (error) {
+      res.statusCode = 404;
+      res.end(error.message);
+    }
+  }
+  res.end();
+});
+	
+app.listen(port);
